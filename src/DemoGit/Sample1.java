10ffdac5869465e5a5f4f65985486910af196032
shubh <shubh@Shubham>
2022-08-04 18:24:52 +0530

    [Shubham Ramteke]= Updated Script

---
+++ src/DemoGit/Sample1.java
@@ -0,0 +1,15 @@
+package DemoGit;
+
+public class Sample1 {
+        public static void main(String[] args) {
+			
+        	System.out.println("hello");
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+		}
+}
